a4d373feb2125f5db779982aedda6b01c9e5d98d
Jaakko Pääkkönen <pkjaa@digitalentshelsinki.internal>
2022-09-09 17:00:56 +0300

    Person schema added

--- server/server.js
+++ server/server.js
@@ -23,7 +23,24 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 })
 
+const personSchema = new mongoose.Schema({
+  name: String,
+  notes: [noteSchema]  
+});
+
+
 const Note = mongoose.model('Note', noteSchema)
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: "Aku Ankka",
+  notes: [{
+    content: 'Aku aloitti',
+    date: new Date('2022-06-17T03:24:00'),
+    important: true
+  }]
+})
 
 const note = [
   new Note({
@@ -49,19 +66,33 @@ note[1].save()
   })
   .catch(err => console.log('pieleen meni'))
 
+person.save()
+  .then(result => {
+    console.log('person saved!')
+  })
+  .catch(err => console.log('pieleen meni'))
+
+
 app.get('/', (req, res) => {
     res.send('Ruori-palvelin...')
 })
 
 app.get('/notes', (req, res) => {
   console.log('Getting the notes...')
-  
-  Note.find({}).then(notes => {
+
+  Person.find({}).then(notes => {
     notes.forEach(note => {
       console.log(note)
     })
     res.json(notes)
   })
+  /*
+  Note.find({}).then(notes => {
+    notes.forEach(note => {
+      console.log(note)
+    })
+    res.json(notes)
+  })*/
 })
 
 const PORT = 5000;
