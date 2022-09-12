cac7508edfd19e923f7a881abf46c96a42f9f102
Jaakko Pääkkönen <pkjaa@digitalentshelsinki.internal>
2022-09-12 17:07:40 +0300

    CRUD added

--- server/server.js
+++ server/server.js
@@ -18,16 +18,19 @@ async function main() {
 }
 
 const noteSchema = new mongoose.Schema({
+  header: String,
   content: String,
   date: Date,
-  important: Boolean,
+  modified: Date,
+  important: Boolean
 })
 
 const personSchema = new mongoose.Schema({
   name: String,
+  email: String,
+  phone: String,
   notes: [noteSchema]  
 });
-
 
 const Note = mongoose.model('Note', noteSchema)
 
@@ -35,43 +38,22 @@ const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
   name: "Aku Ankka",
+  email: "aku313@gmail.com",
+  phone: "030 313 1313",
   notes: [{
-    content: 'Aku aloitti',
+    content: 'Aku Ankka aloitti',
     date: new Date('2022-06-17T03:24:00'),
     important: true
   }]
 })
 
-const note = [
-  new Note({
-    content: 'Asiakas aloitti',
-    date: new Date('2022-08-17T03:24:00'),
-    important: true
-  }),
-  new Note({
-    content: 'Asiakas heitti homman kesken',
-    date: new Date(),
-    important: true
-})];
-
-note[0].save()
-  .then(result => {
-    console.log('note saved!')
-  })
-  .catch(err => console.log('pieleen meni'))
-
-note[1].save()
-  .then(result => {
-    console.log('note saved!')
-  })
-  .catch(err => console.log('pieleen meni'))
-
+/*
 person.save()
   .then(result => {
     console.log('person saved!')
   })
   .catch(err => console.log('pieleen meni'))
-
+*/
 
 app.get('/', (req, res) => {
     res.send('Ruori-palvelin...')
@@ -86,13 +68,89 @@ app.get('/notes', (req, res) => {
     })
     res.json(notes)
   })
-  /*
-  Note.find({}).then(notes => {
-    notes.forEach(note => {
-      console.log(note)
-    })
-    res.json(notes)
-  })*/
+})
+
+app.post('/new_note', (request, response) => {
+  
+  const body = request.body
+
+  //console.log(body)  
+
+  let id = '631ef5229af0af48284fb6c3';
+  Person.findById(id, (err, person) => {
+    if (err){
+      console.log(err);
+    }
+    else {
+      console.log("Result : ", person);
+      person.notes.push(new Note({
+        header: body.header,
+        content: body.new_note,
+        date: new Date(),
+        important: true
+      }));
+      person.save().then(savedNote => {
+        response.json(savedNote)
+      });
+    }
+  });
+
+})
+
+app.post('/update_note', (request, response) => {
+  
+  const body = request.body
+
+  console.log(body.person_id)  
+
+  let id = '631ef5229af0af48284fb6c3';
+  Person.findById(id, function (err, person) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+      const note = person.notes.id(body.note_id);
+      console.log("Note: " + note);
+      if (note) {          
+        note.content = body.updated_note;
+        note.header = body.updated_header;
+        note.modified = new Date();
+        person.save().then(savedNote => {
+          response.json(savedNote)
+        });
+      } else {
+        response.json("Note does not exist!")
+      }        
+    }
+  });
+
+})
+
+app.delete('/delete_note', (request, response) => {
+  const body = request.body;
+
+  console.log(body);
+  //Note.deleteOne({ _id: body.note_id });
+
+  let id = '631ef5229af0af48284fb6c3';
+  Person.findById(id, function (err, person) {
+    if (err){
+        console.log(err);
+    }
+    else {
+        //console.log("Notes : ", person.notes);
+        const note = person.notes.id(body.note_id);
+        //console.log("Note: " + note);
+        if (note) {          
+          note.remove();
+          person.save().then(savedNote => {
+            response.json(savedNote)
+          });
+        } else {
+          response.json("Note does not exist!")
+        }       
+    }
+  });  
 })
 
 const PORT = 5000;
